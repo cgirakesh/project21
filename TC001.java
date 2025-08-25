42241f5abeb614b8ca2a6c04ce25787eed8aad33
cgirakesh <cgirakesh39@gmail.com>
2025-08-25 17:27:12 +0530

    Add files via upload

---
+++ TC001.java
@@ -0,0 +1,23 @@
+package PAC1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC001 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com/");
+		WebElement search = driver.findElement(By.id("APjFqb"));
+		search.sendKeys("Automation Testing tools");
+		search.submit();
+
+    
+    driver.quit();
+}
+}
